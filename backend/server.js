903fe47e3bf55e7a7ad06b2b14233dca65021f5c
harshavardhan774 <harshavardhantingare74@gmail.com>
2026-01-21 19:24:28 +0530

    Initial glassmorphism todo app

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+let todos = [];
+
+/* API */
+app.get("/api/todos", (req, res) => res.json(todos));
+
+app.post("/api/todos", (req, res) => {
+  const todo = { id: Date.now(), text: req.body.text };
+  todos.push(todo);
+  res.json(todo);
+});
+
+app.delete("/api/todos/:id", (req, res) => {
+  todos = todos.filter(t => t.id != req.params.id);
+  res.json({ success: true });
+});
+
+/* Serve React build */
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (_, res) =>
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`Backend running on http://192.168.56.24:${PORT}`)
+);
